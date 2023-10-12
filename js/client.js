2412864877e04b6402542f5b00efda51f46a8828
pawan4593 <mr.pawan_patil@yahoo.com>
2023-10-13 02:02:24 +0530

    created chat app and fixed the cors error

--- js/client.js
+++ js/client.js
@@ -1,2 +1,43 @@
 //Went to socket io documentation
 //See javascript client
+const socket = io('http://localhost:6600') ;
+
+const form = document.getElementById('send-container');
+const messageInput = document.getElementById('messageInput');
+const messageContainer = document.querySelector('.container');
+var audio = new Audio('ting.mp3') ;
+
+
+const append = (msg , position)=>{
+    const msgElement = document.createElement('div') ;
+    msgElement.innerText = msg ;
+    msgElement.classList.add('message') ;
+    msgElement.classList.add(position) ;
+    messageContainer.append(msgElement) ;
+    if(position == 'left')
+        audio.play() ;
+}
+
+form.addEventListener('submit' , (e)=>{
+    e.preventDefault() ;
+    const message = messageInput.value ;
+    append(`You : ${message}` , 'right') ;
+    socket.emit('send-msg' , message) ;
+    messageInput.value = "" ;
+}) ;
+
+
+const name_joiner = prompt("Enter your name to join") ; 
+socket.emit('new-user-joined', name_joiner) ;
+
+socket.on('user-joined' , name_joiner=>{
+    append(`${name_joiner} joined the chat ` , 'left')
+}) ;
+
+socket.on('receive-msg' , data=>{
+    append(`${data.name} : ${data.message} ` , 'left')
+}) ;
+
+socket.on('left' , data=>{
+    append(`${data} has left the chat` , 'left')
+}) ;
--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,24 +1,72 @@
-//This is node server which will handle socket io connections
+// //This is node server which will handle socket io connections
 
-//6600 is port number
-const io = require('socket.io')(6600)
+// //6600 is port number
+// const io = require('socket.io')(6600)
 
-const users = {}
+// const users = {}
 
-//On receiving connection in the socket , run the arrow function ex. Rohan , Divya connected etc like connections are being listened
-io.on('connection' , socket =>{
-    //What happens to particular connection(Rohan , Divya) , decided by socket.io
-    //new-user-joined is an event , this event is user defined and will be handled
-    socket.on('new-user-joined' , name =>{
-        //The moment I get new-user-joined event , the name will be set in the users
-        users[socket.id] = name ;
-        //Let everyone know that user-joined(event) with name except the one who joined
-        socket.broadcast.emit('user-joined' , name) ;
-    });
+// //On receiving connection in the socket , run the arrow function ex. Rohan , Divya connected etc like connections are being listened
+// io.on('connection' , socket =>{
+//     //What happens to particular connection(Rohan , Divya) , decided by socket.io
+//     //new-user-joined is an event , this event is user defined and will be handled
+//     socket.on('new-user-joined' , name =>{
+//         //The moment I get new-user-joined event , the name will be set in the users
+//         users[socket.id] = name ;
+//         //Let everyone know that user-joined(event) with name except the one who joined
+//         socket.broadcast.emit('user-joined' , name) ;
+//     });
 
-    //What to do when chat message send event occurs . send is the event whose name i have decided
-    socket.on('send' , message =>{
-        //recive is an event whose name i have decided
-        socket.broadcast.emit('receive' , {message : message , name : users[socket.id]}) ;
-    });
+//     //What to do when chat message send event occurs . send is the event whose name i have decided
+//     socket.on('send-msg' , message =>{
+//         //recive is an event whose name i have decided
+//         socket.broadcast.emit('receive-msg' , {message : message , name : users[socket.id]}) ;
+//     });
+
+//     socket.on('disconnect' , message =>{
+//         //recive is an event whose name i have decided
+//         socket.broadcast.emit('left' , users[socket.id]) ;
+//         delete users[socket.io] ;
+//     });
+
+
+
+// });
+
+
+
+const http = require('http');
+const socketIo = require('socket.io');
+
+const server = http.createServer((req, res) => {
+
+});
+
+
+const io = socketIo(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+});
+
+const users = {};
+
+io.on('connection', (socket) => {
+  socket.on('new-user-joined', (name) => {
+    users[socket.id] = name;
+    socket.broadcast.emit('user-joined', name);
+  });
+
+  socket.on('send-msg', (message) => {
+    socket.broadcast.emit('receive-msg', { message: message, name: users[socket.id] });
+  });
+
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('left', users[socket.id]);
+    delete users[socket.id];
+  });
+});
+
+server.listen(6600, () => {
+  console.log('Server is running on port 6600');
 });
